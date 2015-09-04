cc2d60ea7478c37456d4f7ed6b1df230ad9a5db8
Fouad Masoud <fouad.fm@gmail.com>
2015-09-04 10:07:26 +0400

    first commit

---
+++ db/migrate/20150825125328_create_listings.rb
@@ -0,0 +1,10 @@
+class CreateListings < ActiveRecord::Migration
+  def change
+    create_table :listings do |t|
+      t.string :name
+      t.string :description
+      t.decimal :price
+      t.timestamps null: false
+    end
+  end
+end
